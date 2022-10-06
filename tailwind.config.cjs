207b97adc5647e91609886259a627bd64bce2661
Bertil-Bjoernboel <bert5659@stud.kea.dkgit config --global user.email Bert5659@stud.kea.dk>
2022-10-06 09:48:03 +0200

    Custom Farver Indsat

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        creme: "#F7C892",
+        orange: "#FF6434",
+        lilla: "#6432C9",
+        lyslilla: "#E1CDFF",
+        brun: "#A06E29",
+        lyscreme: "#E8E1D1",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
