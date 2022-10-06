87c3ad705023b2fa39cdef6638e642698d87fcf9
Louise Thomsen <loui718d@stud.kea.dk>
2022-10-06 12:57:44 +0200

    splash

--- src/main.js
+++ src/main.js
@@ -2,3 +2,17 @@ import "./style.css";
 import Alpine from "alpinejs";
 window.Alpine = Alpine;
 Alpine.start();
+
+const openNav = document.querySelector(".openbtn");
+const closeNav = document.querySelector(".closebtn");
+
+openNav.addEventListener("click", open);
+closeNav.addEventListener("click", close);
+function open() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function close() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
