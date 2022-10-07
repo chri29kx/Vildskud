a11c583130ebdece187de435d2f479cd97149c02
Louise Thomsen <loui718d@stud.kea.dk>
2022-10-07 13:02:14 +0200

    nav

--- src/main.js
+++ src/main.js
@@ -5,14 +5,27 @@ Alpine.start();
 
 const openNav = document.querySelector(".openbtn");
 const closeNav = document.querySelector(".closebtn");
+const myNav = document.getElementById("stickyhead");
 
 openNav.addEventListener("click", open);
 closeNav.addEventListener("click", close);
 function open() {
-  document.getElementById("mySidepanel").style.width = "450px";
+  document.getElementById("mySidepanel").style.width = "100%";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function close() {
   document.getElementById("mySidepanel").style.width = "0";
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    myNav.style.display = "block";
+  } else {
+    myNav.style.display = "none";
+  }
 }
