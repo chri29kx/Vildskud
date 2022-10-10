5a1180b6b89598aee656d1ef58fdc3594a88b268
Louise Thomsen <loui718d@stud.kea.dk>
2022-10-10 13:37:31 +0200

    header

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        events: resolve(__dirname, "src/events.html"),
+        find: resolve(__dirname, "src/find_os.html"),
+        kontakt: resolve(__dirname, "src/om_kontakt.html"),
+        vaermed: resolve(__dirname, "src/om_vaermed.html"),
+        vildskud: resolve(__dirname, "src/om_vildskud.html"),
+        program: resolve(__dirname, "src/program.html"),
+        singleview: resolve(__dirname, "src/singleview.html"),
         // about: resolve(__dirname, "src/about.html"),
         // skriv liste med sider her
       },
