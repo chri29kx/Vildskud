ff8c097884fa16ba95835b6cffee779ed1cbcedf
Bertil-Bjoernboel <bert5659@stud.kea.dkgit config --global user.email Bert5659@stud.kea.dk>
2022-10-12 13:04:16 +0200

    Cookiebox

--- src/main.js
+++ src/main.js
@@ -29,3 +29,19 @@ function scrollFunction() {
     myNav.style.display = "none";
   }
 }
+
+const acceptCookie = document.querySelector("#accepter");
+const denyCookie = document.querySelector("#afvis");
+const cookieboks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieBoks);
+denyCookie.addEventListener("click", fjernCookieBoks);
+
+function fjernCookieBoks() {
+  cookieboks.classList.add("hidden");
+  sessionStorage.setItem("fjerncookieboks", true);
+}
+
+if (sessionStorage.getItem("fjerncookieboks")) {
+  cookieboks.classList.add("hidden");
+}
